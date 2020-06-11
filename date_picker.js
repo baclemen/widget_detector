8491773bf8a1e3538c7b5a8f876dd96adb6a46b7
baclemen <sw4rgvs.y0l0nymous>
2020-06-11 16:44:25 +0200

    first commit

---
+++ date_picker.js
@@ -0,0 +1,9 @@
+console.log("Chrome Extension works");
+const dateselector1 = document.querySelectorAll('input[class*=date]')
+const dateselector2 = document.querySelectorAll('input[class*=Date]')
+const dateselector3 = document.querySelectorAll('input[id*=date]')
+const dateselector4 = document.querySelectorAll('input[id*=Date]')
+
+let dateselector = dateselector1
+
+console.log(dateselector);
