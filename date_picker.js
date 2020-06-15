566e194aace9cfc2c6bf5ec6748d9aa5b7331751
baclemen <sw4rgvs.y0l0nymous>
2020-06-15 09:03:26 +0200

    multiple date pickers

--- date_picker.js
+++ date_picker.js
@@ -3,9 +3,9 @@ console.log("Chrome Extension works");
 const head =document.querySelector('head');
 
 
-var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
-head.appendChild(script);
+// var script = document.createElement('script');
+// script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+// head.appendChild(script);
 //add jquery
 // console.log(head);
 // head.appendChild('<script src="https://code.jquery.com/jquery-3.5.0.js"></script>');
@@ -14,6 +14,7 @@ head.appendChild(script);
 
 //select all datepicker objects on the website
 const dateselectorlist = document.querySelectorAll('input[class*=date], input[class*=Date], input[id*=date], input[id*=Date]');
+console.log(dateselectorlist);
 // const dateselector2 = document.querySelectorAll('input[class*=Date]');
 // const dateselector3 = document.querySelectorAll('input[id*=date]');
 // const dateselector4 = document.querySelectorAll('input[id*=Date]');
@@ -28,11 +29,12 @@ function setDate(event){ //TODO: make this possible for any amount of date picke
     console.log("setDate");
     console.log("nodelist:", dateselectorlist.item(0));
     console.log(event);
+    i =event.srcElement.id.match(/\d+/)[0];
     //dateselectorlist.item(0).setAttribute("value","1.1.2020"); 
     dateselectorlist.item(0).click();
     // var input = $('input[class*=date], input[class*=Date], input[id*=date], input[id*=Date]');
     // input[0].selectionStart = input[0].selectionEnd = input.val().length;
-    $("input[class*=date], input[class*=Date], input[id*=date], input[id*=Date]").first().val("1.1.2020");
+    $("input[class*=date], input[class*=Date], input[id*=date], input[id*=Date]").eq(i).val("1.1.2020");
 }
 
 
@@ -47,6 +49,7 @@ for(var i = 0; i < dateselectorlist.length; i++) {
     btn.innerHTML = "setDate";
     btn.className = "detectorbutton";
     btn.id ="detectorbutton"+i;
+    btn.number = i;
     btn.onclick = function(event){event.preventDefault(); console.log("click"); setDate(event)}
     dateselectorlist.item(i).parentNode.appendChild(btn);
     console.log("button added");
